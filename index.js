7d8d1551be2064a6f632dbcd22c4ab0a55875041
dwiramansyah <73238243+dwiramansyah@users.noreply.github.com>
2020-12-20 22:26:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://spot-leaf-taleggio.glitch.me"]
+const urls = ["https://wii-mus.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
